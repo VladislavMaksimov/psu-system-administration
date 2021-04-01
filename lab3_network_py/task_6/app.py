996d4fa11beec8356e5e3993a164f355653d4914
VladislavMaksimov <workmaximov@yandex.ru>
2021-04-01 19:19:35 +0500

    Change timeout.

--- lab3_network_py/task_6/app.py
+++ lab3_network_py/task_6/app.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+from threading import Thread
 
 # берёт ip из аргумента командной строки
 if len(sys.argv) == 2:
@@ -15,16 +16,19 @@ else:
 print("Сканирую: " + ip)
 print()
 
+def scan_port(scanner, ip, port):
+    try:
+        connect = scanner.connect((ip,port))
+        print('Порт {} открыт'.format(port))
+        connect.close()
+    except:
+        pass
+
 try:
     for port in range(1,65535):
         scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        scanner.settimeout(0.5)
-        try:
-            connect = scanner.connect((ip,port))
-            print('Порт {} открыт'.format(port))
-            connect.close()
-        except:
-            pass
+        scanner.settimeout(0.0001)
+        scan_port(scanner, ip, port)
 except KeyboardInterrupt:
         print("Программа завершена пользователем.")
         sys.exit()

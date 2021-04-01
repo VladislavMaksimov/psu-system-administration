62bbd71b0f6941e371a0944722786d2a0f76a311
VladislavMaksimov <workmaximov@yandex.ru>
2021-04-02 02:16:31 +0500

    Add 'list' method for task 1 from lab 3.

--- lab3_network_py/task_1/handler.py
+++ lab3_network_py/task_1/handler.py
@@ -1,4 +1,5 @@
 from socketserver import BaseRequestHandler
+import os
 
 class FileHandler(BaseRequestHandler):
     methods = ['auth', 'list', 'info', 'retr', 'help']
@@ -7,6 +8,15 @@ class FileHandler(BaseRequestHandler):
         help_str = "auth user pass — авторизация, user и pass хранятся в файле pass в каталоге запуска программы\nlist — показать список файлов в каталоге запуска программы\ninfo file — напечатать сведения о файле, mime тип, размер, время создания\nretr file1 file2 file_n — передать файлы, указанные в строке\nexit — выход\nhelp — справка по командам"
         self.request.sendall(bytes(help_str, 'utf8'))
 
+    def list(self):
+        directory = os.listdir('.')
+        files = ' '.join(filter(os.path.isfile, directory))
+        if files != '':
+            self.request.sendall(bytes(files, 'utf8'))
+        else:
+            self.request.sendall(bytes('файлов нет', 'utf8'))
+
+    # определяет поведение сервера
     def handle(self):
         while True:
             data = self.request.recv(1024).decode()
@@ -21,3 +31,6 @@ class FileHandler(BaseRequestHandler):
 
             if method == 'help':
                 self.help()
+
+            if method == 'list':
+                self.list()
